16fba43a6d14d889889ae9fa458b3274b1d037ad
Alexis <alexis.mathiot@gmail.com>
2020-11-04 20:51:42 +0100

    retouche signin.php création front  profile.php

--- profile.php
+++ profile.php
@@ -7,37 +7,30 @@ require 'includes/navbar.php';
     <div class="columns">
         <div class="column">
             <div class="card p-2">
-                <div class="file">
-                    <label class="file-label">
-                        <input class="file-input" type="file" name="resume">
-                        <span class="file-cta">
-      <span class="file-icon">
-        <i class="fas fa-upload"></i>
-      </span>
-      <span class="file-label">
-        Choose a file…
-      </span>
-    </span>
-                    </label>
-                </div>
-                <h2 class="title">Mon profil</h2>
+                <h2 class="title has-text-centered">Mon profil</h2>
                 <div class="card-content">
                     <div class="media">
                         <div class="media-left">
-                            <figure class="image is-128x128">
-                                <img src="https://bulma.io/images/placeholders/128x128.png">
+                            <figure class="image is-128x128 is-3by4">
+                                <img src="images/photoprofil.jpg" alt="image profile">
                             </figure>
                         </div>
-                        <div class="media-content">
+                        <div class="media-content has-text-centered">
                             <p class="title is-4"><?= $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></p>
                             <p class="subtitle is-6"><?= $_SESSION['email'] ?></p>
                         </div>
                     </div>
                 </div>
-                <div class="buttons">
-                    <button class="button is-primary">Primary</button>
-                    <button class="button is-link">Link</button>
-                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="buttons">
+                <a class="button is-link">Modifier mon profil</a>
+                <a class="button is-link">Publier une nouvelle annonce</a>
+            </div>
+            <div class="buttons">
+                <a class="button is-link">Voir mes annonces</a>
+                <a class="button is-link">Voir mes réservations</a>
             </div>
         </div>
     </div>
